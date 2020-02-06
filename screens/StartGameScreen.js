39eab32601156ea6a854c647969aaa98a1271177
ryanstgermain <ryanjoel26@gmail.com>
2020-02-06 16:36:23 -0700

    Added validations for the number input.

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -14,9 +14,32 @@ import Input from '../components/Input';
 
 const StartGameScreen = (props) => {
   const [ enteredValue, setEnteredValue ] = useState('');
+  const [ confirmed, setConfirmed ] = useState(false);
+  const [ selectedNumber, setSelectedNumber ] = useState();
 
   const numberInputHandler = (inputText) => {
     setEnteredValue(inputText.replace(/[^0-9]/g, ''));
+  };
+
+  const resetInputHandler = () => {
+    setEnteredValue('');
+    setConfirmed(false);
+  };
+
+  const confirmInputHandler = () => {
+    const chosenNumber = parseInt(enteredValue);
+    if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99) {
+      return;
+    }
+    setConfirmed(true);
+    setSelectedNumber(chosenNumber);
+    setEnteredValue('');
+  };
+
+  let confirmedOutput;
+
+  if (confirmed) {
+    confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>
   }
 
   return (
@@ -43,22 +66,19 @@ const StartGameScreen = (props) => {
               <Button
                 title='Confirm'
                 color='black'
-                onPress={() => {
-                    
-                }}
+                onPress={confirmInputHandler}
               />
             </View>
             <View style={styles.button}>
               <Button
                 title='Reset'
                 color={colors.accent}
-                onPress={() => {
-                    
-                }}
+                onPress={resetInputHandler}
               />
             </View>
           </View>
         </Card>
+        {confirmedOutput}
       </View>
     </TouchableWithoutFeedback>
   );

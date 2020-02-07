61401f2db9dc5d78ae519172610c1c5d1f27fbdc
ryanstgermain <ryanjoel26@gmail.com>
2020-02-07 12:04:02 -0700

    Got to lower and greater buttons to work while in a game.

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import {
   StyleSheet,
   View,
   Text,
-  Button
+  Button,
+  Alert
 } from 'react-native';
 // import colors from '../constants/colors';
 
@@ -25,6 +26,28 @@ const GameScreen = (props) => {
   const [ currentGuess, setCurrentGuess ] = useState(
     generateRandomBetween(1, 100, props.userChoice)
   );
+  const currentLow = useRef(1);
+  const currentHigh = useRef(100);
+
+  const nextGuessHandler = (direction) => {
+    if ((direction === 'lower' && currentGuess < props.userChoice) || (direction === 'greater' && currentGuess > props.userChoice)) {
+      Alert.alert(
+        'Don\'t lie!',
+        'You know that this is wrong...',
+        [{ text: 'Sorry!', style: 'cancel' }]
+      );
+      return;
+    };
+
+    if (direction === 'lower') {
+      currentHigh.current = currentGuess;
+    } else {
+      currentLow.current = currentGuess;
+    };
+
+    const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
+    setCurrentGuess(nextNumber);
+  };
 
   return (
     <View style={styles.screen}>
@@ -36,16 +59,12 @@ const GameScreen = (props) => {
         <Button
           title='LOWER'
           color='black'
-          onPress={() => {
-
-          }}
+          onPress={nextGuessHandler.bind(this, 'lower')}
         />
         <Button
           title='GREATER'
           color='black'
-          onPress={() => {
-              
-          }}
+          onPress={nextGuessHandler.bind(this, 'greater')}
         />
       </Card>
     </View>

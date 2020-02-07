a5bf10679e4cdd9c648a49505f94f69555bd4f0b
ryanstgermain <ryanjoel26@gmail.com>
2020-02-07 14:42:42 -0700

    Finished game logic.

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button
+} from 'react-native';
 
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text>The Game is Over!</Text>
+      <Text>Number of rounds: {props.numberOfRounds}</Text>
+      <Text>Number chosen was: {props.userNumber}</Text>
+      <Button
+        title='NEW GAME'
+        color='black'
+        onPress={props.onRestart}
+      />
     </View>
   );
 };

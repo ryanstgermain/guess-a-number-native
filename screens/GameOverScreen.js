27fb24fa148ee8c6200510c219c982523f114866
ryanstgermain <ryanjoel26@gmail.com>
2020-02-07 16:07:17 -0700

    Added an image to the game over page.

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { StyleSheet, View, Button } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Button,
+  Image
+} from 'react-native';
 import colors from '../constants/colors';
 
 import TitleText from '../components/TitleText';
@@ -9,8 +14,15 @@ const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
       <TitleText>The Game is Over!</TitleText>
-      <BodyText>Number of rounds: {props.numberOfRounds}</BodyText>
+      <View style={styles.imageContainer}>
+       <Image
+          source={require('../assets/success.png')}
+          style={styles.image}
+          resizeMode='cover'
+        /> 
+      </View>
       <BodyText>Number chosen was: {props.userNumber}</BodyText>
+      <BodyText>Number of rounds: {props.numberOfRounds}</BodyText>
       <Button
         title='NEW GAME'
         color={colors.accent}
@@ -25,6 +37,21 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: colors.primary,
+    overflow: 'hidden',
+    marginVertical: 30
+  },
+
+  image: {
+    width: '100%',
+    height: '100%'
   }
 });
 

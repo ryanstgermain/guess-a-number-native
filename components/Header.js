6d954866c7ae7ddce927d9b95c124c403943bdf4
ryanstgermain <ryanjoel26@gmail.com>
2020-02-13 10:59:14 -0700

    Added styling conditions for platforms.

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Platform } from 'react-native';
 import colors from '../constants/colors';
 
 import TitleText from '../components/TitleText';
@@ -19,13 +19,15 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 90,
     paddingTop: 36,
-    backgroundColor: colors.primary,
+    backgroundColor: Platform.OS === 'android' ? colors.primary : 'white',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
+    borderBottomWidth: Platform.OS === 'ios' ? 1 : 0
   },
 
   headerTitle: {
-    color: 'white'
+    color: Platform.OS === 'android' ? 'white' : colors.accent
   }
 });
 
